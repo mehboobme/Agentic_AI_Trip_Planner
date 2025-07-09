5ef5d6a4f7b9ac72da37ea0642df7f3f74ba2daa
Mahboob Khan <mehboobmeo@email.com>
2025-07-09 09:34:29 +0300

    updated utils

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -17,7 +17,7 @@ class ConfigLoader:
 
 class ModelLoader(BaseModel):
     model_provider: Literal["groq", "openai"] = "groq"
-    config: Optional[ConfigLoader] = Field(default=None, exclude=True)
+    config: Optional[ConfigLoader] = Field(default=None, exclude=True) 
 
     def model_post_init(self, __context: Any) -> None:
         self.config = ConfigLoader()
